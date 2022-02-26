0eb78358ddb4a2577dab2bcca041c5d3f2f6809a
Estebanph22 <up4968834@gmail.com>
2022-02-26 15:55:20 -0600

    actividad 26 feb

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  /*nombres=["Abraham Ramirez", "Brian Esquivel", "Diego Davila",
+"Diego Jasso", "Julio Manuel", "Luis Armando Villanueva", "Luis Otoniel Tamez", "Manuel Antonio Juarez","Rogel Axel", "Sergio Gutierrez", "Omar Garza"];*/
+alumnos=[
+{
+  nombre:"Abraham",
+  apellido:"Ramirez",
+  matricula:"111"
+},
+{
+  nombre:"Brian",
+  apellido:"Esquivel",
+  matricula:"222"
+},
+{
+  nombre:"Diego",
+  apellido:"Davila",
+  matricula:"333"
+},
+{
+  nombre:"Diego",
+  apellido:"Jasso",
+  matricula:"444"
+},
+{
+  nombre:"Julio",
+  apellido:"Manuel",
+  matricula:"555"
+},
+{
+  nombre:"Armando",
+  apellido:"Villanueva",
+  matricula:"555"
+},
+{
+  nombre:"Luis",
+  apellido:"Tamez",
+  matricula:"555"
+},
+{
+  nombre:"Manuel",
+  apellido:"Juarez",
+  matricula:"555"
+},
+{
+  nombre:"Rogel",
+  apellido:"Axel",
+  matricula:"555"
+},
+{
+  nombre:"Sergio",
+  apellido:"Gutierrez",
+  matricula:"555"
+},
+{
+  nombre:"Omar",
+  apellido:"Garza",
+  matricula:"555"
+},
+]
   constructor() {}
 
 }
