c127621374d36cf33479f93666179566857c93e8
Estebanph22 <up4968834@gmail.com>
2022-03-26 15:54:17 -0600

    practica 6-sliding y action sheet

--- src/app/alumno-detalle/alumno-detalle.component.ts
+++ src/app/alumno-detalle/alumno-detalle.component.ts
@@ -3,7 +3,7 @@
 /* eslint-disable @typescript-eslint/member-ordering */
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-alumno-detalle',
   templateUrl: './alumno-detalle.component.html',
@@ -11,7 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AlumnoDetalleComponent implements OnInit {
 
-  constructor(private ruta: ActivatedRoute) { }
+  constructor(private ruta: ActivatedRoute,public actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
     this.obtenerDetalleAlumno(this.matricula);
@@ -85,5 +85,55 @@ export class AlumnoDetalleComponent implements OnInit {
         }
       }
       return this.alumnoDetalle;
+    }
+    async presentActionSheet() {
+      const actionSheet = await this.actionSheetController.create({
+        header: 'Albums',
+        cssClass: 'my-custom-class',
+        mode: 'ios', 
+        buttons: [{
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          id: 'delete-button',
+          data: {
+            type: 'delete'
+          },
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        }, {
+          text: 'Share',
+          icon: 'share',
+          data: 10,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        }, {
+          text: 'Play (open modal)',
+          icon: 'caret-forward-circle',
+          data: 'Data value',
+          handler: () => {
+            console.log('Play clicked');
+          }
+        }, {
+          text: 'Favorite',
+          icon: 'heart',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }]
+      });
+      await actionSheet.present();
+  
+      const { role, data } = await actionSheet.onDidDismiss();
+      console.log('onDidDismiss resolved with role and data', role, data);
     }
 }
